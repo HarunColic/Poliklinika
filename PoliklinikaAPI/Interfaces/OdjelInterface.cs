78a6f49994eb7be58641febbff0a6d32df44d608
Fahrizada Zekotić <fahrizada.zekotic@edu.fit.ba>
2021-04-27 23:25:54 +0200

    interface odjel

---
+++ PoliklinikaAPI/Interfaces/OdjelInterface.cs
@@ -0,0 +1,17 @@
+﻿using Poliklinika.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoliklinikaAPI.Interfaces
+{
+    public interface OdjelInterface
+    {
+        public IList<Odjel> GetAll();
+        public Odjel Get();
+        public Odjel Create();
+        public Odjel Update();
+        public Odjel Delete();
+    }
+}
