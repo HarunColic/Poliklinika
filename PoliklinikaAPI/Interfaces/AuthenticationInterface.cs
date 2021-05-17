64ca87439c75e0773acf1330c69893f7e4160ef2
HarunColic <haruncolic@hotmail.com>
2021-05-17 21:10:43 +0200

    Dodana sesija preko JWT

---
+++ PoliklinikaAPI/Interfaces/AuthenticationInterface.cs
@@ -0,0 +1,15 @@
+﻿using Poliklinika.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PoliklinikaAPI.ViewModels;
+
+namespace PoliklinikaAPI.Interfaces
+{
+    public interface AuthenticationInterface
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        User GetById(int id);
+    }
+}
