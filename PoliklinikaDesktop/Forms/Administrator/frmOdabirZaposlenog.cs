614dc489c42a2dffb74728b434db0b787a0cdd3e
Fahrizada Zekotić <fahrizada.zekotic@edu.fit.ba>
2021-05-18 00:26:36 +0200

    dodavanje tehnicara

--- PoliklinikaDesktop/Forms/Administrator/frmOdabirZaposlenog.cs
+++ PoliklinikaDesktop/Forms/Administrator/frmOdabirZaposlenog.cs
@@ -1,6 +1,7 @@
 ﻿using Poliklinika.Model;
 using PoliklinikaAPI.ViewModels;
 using PoliklinikaDesktop.Forms.Doktor;
+using PoliklinikaDesktop.Forms.Tehnicar;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -26,10 +27,16 @@ namespace PoliklinikaDesktop.Forms.Administrator
             doktor.Show();
         }
 
+        private void picDodajTehnicara_Click(object sender, EventArgs e)
+        {
+            frmDetaljiTehnicar doktor = new frmDetaljiTehnicar();
+            doktor.Show();
+        }
         private void frmOdabirZaposlenog_Load(object sender, EventArgs e)
         {
-           var zaposlenici = _serviceDoktor.Get<IList<DoktorVM>>()
-               + _serviceTehnicar.Get<IList<TehnicarVM>>();
+           
         }
+
+       
     }
 }
