f5f335fd57c6c903d9ea5dc50525efc17948254a
Fahrizada Zekotić <fahrizada.zekotic@edu.fit.ba>
2021-05-18 22:57:22 +0200

    dodane stvari za nalaz

---
+++ PoliklinikaAPI/Interfaces/NalazInterface.cs
@@ -0,0 +1,18 @@
+﻿using Poliklinika.Model;
+using PoliklinikaAPI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoliklinikaAPI.Interfaces
+{
+    public interface NalazInterface
+    {
+        public List<Nalaz> GetAll();
+        public Nalaz Get(int id);
+        public Nalaz Insert(NalazVM nalaz);
+        public Nalaz Update(int id, NalazVM nalaz);
+        public void Delete(int id);
+    }
+}
---
+++ PoliklinikaDesktop/Forms/Nalaz/frmDetaljiNalaz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PoliklinikaDesktop.Forms.Nalaz
+{
+    public partial class frmDetaljiNalaz : Form
+    {
+        public frmDetaljiNalaz()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDetaljiNalaz_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+       
+    }
+}
