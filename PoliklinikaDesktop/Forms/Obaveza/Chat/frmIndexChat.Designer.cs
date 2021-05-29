ddcf6fda83a04bb8654b7449b9e4f7076ae54703
HarunColic <haruncolic@hotmail.com>
2021-05-29 16:29:33 +0200

    dorada chata

--- PoliklinikaDesktop/Forms/Obaveza/Chat/frmIndexChat.Designer.cs
+++ PoliklinikaDesktop/Forms/Obaveza/Chat/frmIndexChat.Designer.cs
@@ -31,43 +31,45 @@ namespace PoliklinikaDesktop.Forms.Obaveza.Chat
         {
             this.txtPoruka = new System.Windows.Forms.RichTextBox();
             this.btnPosalji = new System.Windows.Forms.Button();
-            this.flwChat = new System.Windows.Forms.FlowLayoutPanel();
+            this.tblLayout = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
             // txtPoruka
             // 
             this.txtPoruka.Location = new System.Drawing.Point(12, 361);
             this.txtPoruka.Name = "txtPoruka";
-            this.txtPoruka.Size = new System.Drawing.Size(621, 62);
+            this.txtPoruka.Size = new System.Drawing.Size(751, 62);
             this.txtPoruka.TabIndex = 0;
             this.txtPoruka.Text = "";
             // 
             // btnPosalji
             // 
-            this.btnPosalji.Location = new System.Drawing.Point(669, 376);
+            this.btnPosalji.Location = new System.Drawing.Point(799, 375);
             this.btnPosalji.Name = "btnPosalji";
             this.btnPosalji.Size = new System.Drawing.Size(94, 29);
             this.btnPosalji.TabIndex = 1;
             this.btnPosalji.Text = "Pošalji";
             this.btnPosalji.UseVisualStyleBackColor = true;
             // 
-            // flwChat
+            // tblLayout
             // 
-            this.flwChat.AutoScroll = true;
-            this.flwChat.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flwChat.Location = new System.Drawing.Point(12, 35);
-            this.flwChat.Name = "flwChat";
-            this.flwChat.Size = new System.Drawing.Size(751, 305);
-            this.flwChat.TabIndex = 2;
+            this.tblLayout.AutoScroll = true;
+            this.tblLayout.ColumnCount = 1;
+            this.tblLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblLayout.Location = new System.Drawing.Point(12, 12);
+            this.tblLayout.Name = "tblLayout";
+            this.tblLayout.RowCount = 1;
+            this.tblLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tblLayout.Size = new System.Drawing.Size(881, 332);
+            this.tblLayout.TabIndex = 3;
             // 
             // frmIndexChat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.flwChat);
+            this.ClientSize = new System.Drawing.Size(924, 450);
+            this.Controls.Add(this.tblLayout);
             this.Controls.Add(this.btnPosalji);
             this.Controls.Add(this.txtPoruka);
             this.Name = "frmIndexChat";
@@ -81,6 +83,6 @@ namespace PoliklinikaDesktop.Forms.Obaveza.Chat
 
         private System.Windows.Forms.RichTextBox txtPoruka;
         private System.Windows.Forms.Button btnPosalji;
-        private System.Windows.Forms.FlowLayoutPanel flwChat;
+        private System.Windows.Forms.TableLayoutPanel tblLayout;
     }
 }
--- PoliklinikaDesktop/Forms/Obaveza/Chat/frmIndexChat.cs
+++ PoliklinikaDesktop/Forms/Obaveza/Chat/frmIndexChat.cs
@@ -21,23 +21,40 @@ namespace PoliklinikaDesktop.Forms.Obaveza.Chat
 
         private async void frmIndexChat_Load(object sender, EventArgs e)
         {
-            
+            tblLayout.AutoScroll = false;
+            tblLayout.HorizontalScroll.Enabled = false;
+            tblLayout.HorizontalScroll.Visible = false;
+            tblLayout.AutoScroll = true;
+
             //var chatObaveza = new ChatObavezaVM { ID = int.Parse(_id.ToString()) };
             var chatObaveza = new ChatObavezaVM { ID = 1 };
             var poruke = await _service.Get<List<ChatPorukaVM>>(chatObaveza);
 
-            foreach(var i in poruke)
+            //tblLayout.Controls.Remove();
+
+            foreach (var i in poruke)
             {
                 var lbl = new Label { Text = i.Poruka };
-                lbl.AutoSize = true;
-                lbl.Font = new Font("Arial", 12, FontStyle.Bold);
-
+                lbl.MaximumSize = new Size(200, int.MaxValue);
+                var nesto = lbl.MaximumSize;
+                lbl.Font = new Font("Arial", 12, FontStyle.Regular);
                 //if(CurrentUser.User.Id == i.OsobljeID)
                 //    lbl.Anchor = (AnchorStyles.Top | AnchorStyles.Right);
                 //else
                 //    lbl.Anchor = (AnchorStyles.Top | AnchorStyles.Left);
+                //flwChat.Controls.Add(lbl);
 
-                flwChat.Controls.Add(lbl);
+                if (i.OsobljeID == 16)
+                {
+                    lbl.Anchor = (AnchorStyles.Top | AnchorStyles.Right);
+                    lbl.Padding = new Padding { Right = 30 };
+                }
+                else
+                {
+                    lbl.Anchor = (AnchorStyles.Top | AnchorStyles.Left);
+                    lbl.Padding = new Padding { Left = 30 };
+                }
+                tblLayout.Controls.Add(lbl);
             }
         }
     }
