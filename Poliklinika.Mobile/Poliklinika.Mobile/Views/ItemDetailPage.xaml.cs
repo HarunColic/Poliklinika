27bbfd2d6c59d979fd7aa046ff051e8106c9592c
Fahrizada Zekotić <fahrizada.zekotic@edu.fit.ba>
2021-06-01 19:19:47 +0200

    mobilna app

---
+++ Poliklinika.Mobile/Poliklinika.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Poliklinika.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Poliklinika.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
