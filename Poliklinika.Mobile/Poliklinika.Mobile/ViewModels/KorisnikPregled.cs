4b2c44210657393f2bdb8ab812bcb4336ff5c931
Fahrizada Zekotić <fahrizada.zekotic@edu.fit.ba>
2021-06-11 00:10:50 +0200

    pregled pregleda

---
+++ Poliklinika.Mobile/Poliklinika.Mobile/ViewModels/KorisnikPregled.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Poliklinika.Mobile.ViewModels
+{
+    public class KorisnikPregled : BaseViewModel
+    {
+        private readonly APIService _pregled = new APIService("Pregled");
+        public KorisnikPregled()
+        {
+            InitCommand = new Command(async () => await Init());
+        }
+
+        public ObservableCollection<PreglediVM> PreglediLista { get; set; } = new ObservableCollection<PreglediVM>();
+        public ICommand InitCommand { get; }
+
+
+        public async Task Init()
+        {
+
+            var list = await _pregled.Get<IEnumerable<PreglediVM>>(null);
+            PreglediLista.Clear();
+
+            foreach (var pregledi in list)
+            {
+                if(pregledi.KorisnikID==CurrentUser.User.Id)
+                PreglediLista.Add(pregledi);
+            }
+        }
+        public class PreglediVM
+        {
+            public int KorisnikID { get; set; }
+            public DateTime Datum { get; set; }
+            public int OdjelID { get; set; }
+        }
+    }
+}
