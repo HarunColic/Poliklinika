fadddba44e7a01dceb4f979a24630662d071449d
Fahrizada Zekotić <fahrizada.zekotic@edu.fit.ba>
2021-06-11 16:05:02 +0200

    dodan page za prikaz nalaza

--- Poliklinika.Mobile/Poliklinika.Mobile/Views/PrikazPregledaPage.xaml.cs
+++ Poliklinika.Mobile/Poliklinika.Mobile/Views/PrikazPregledaPage.xaml.cs
@@ -25,5 +25,10 @@ namespace Poliklinika.Mobile.Views
             base.OnAppearing();
 
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PrikazNalazaPage());
+        }
     }
 }
