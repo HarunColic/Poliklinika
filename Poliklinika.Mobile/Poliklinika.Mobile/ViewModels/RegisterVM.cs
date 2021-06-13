2d25cac337f0ee0364d9a6c97b1e5f4101cd3ff6
Fahrizada Zekotić <fahrizada.zekotic@edu.fit.ba>
2021-06-13 17:41:01 +0200

    promjena register page i vm

--- Poliklinika.Mobile/Poliklinika.Mobile/ViewModels/RegisterVM.cs
+++ Poliklinika.Mobile/Poliklinika.Mobile/ViewModels/RegisterVM.cs
@@ -28,7 +28,24 @@ namespace Poliklinika.Mobile.ViewModels
             set { SetProperty(ref _password, value); }
         }
 
-
+        string _ime = string.Empty;
+        public string Ime
+        {
+            get { return _ime; }
+            set { SetProperty(ref _ime, value); }
+        }
+        string _prezime = string.Empty;
+        public string Prezime
+        {
+            get { return _prezime; }
+            set { SetProperty(ref _prezime, value); }
+        }
+        DateTime _datumRodjenja;
+        public DateTime DatumRodjenja
+        {
+            get { return _datumRodjenja; }
+            set { SetProperty(ref _datumRodjenja, value); }
+        }
         public RegisterVM()
         {
             RegisterCommand = new Command(OnRegisterClicked);
@@ -47,7 +64,12 @@ namespace Poliklinika.Mobile.ViewModels
                 var req = new request
                 {
                     email = this.Username,
-                    password = this.Password
+                    password = this.Password,
+                    Ime=this.Ime,
+                    Prezime=this.Prezime,
+                    DatumRodjenja=this.DatumRodjenja
+                  
+                    
                 };
 
                 try
@@ -69,6 +91,9 @@ namespace Poliklinika.Mobile.ViewModels
         {
             public string email { get; set; }
             public string password { get; set; }
+            public string Ime { get; set; }
+            public string Prezime { get; set; }
+            public DateTime DatumRodjenja { get; set; }
         }
 
         public class response
