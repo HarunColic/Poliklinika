6c59069a2e5ece9ffc966aa49ea7ff2b21f107b9
Fahrizada Zekotić <fahrizada.zekotic@edu.fit.ba>
2021-06-17 21:34:50 +0200

    obaveze osoblje ispravljene

--- PoliklinikaDesktop/Forms/Obaveza/frmIndexOsobljeObaveza.cs
+++ PoliklinikaDesktop/Forms/Obaveza/frmIndexOsobljeObaveza.cs
@@ -14,6 +14,7 @@ namespace PoliklinikaDesktop.Forms.Obaveza
         private readonly APIService _service = new APIService("Obaveza");
         private readonly APIService _admin = new APIService("Admin");
         private readonly APIService _chatObaveza = new APIService("ChatObaveza");
+        private readonly APIService _odjel = new APIService("Odjel");
 
 
         public frmIndexOsobljeObaveza()
@@ -41,6 +42,26 @@ namespace PoliklinikaDesktop.Forms.Obaveza
             dgvAktivne.AutoGenerateColumns = false;
             dgvAktivne.DataSource = result;
             dgvIzvrsene.DataSource = result2;
+
+            if (result != null)
+            {
+                foreach (DataGridViewRow i in dgvAktivne.Rows)
+                {
+                    var prid = await _odjel.GetById<OdjelVM>(result[i.Index].OdjelID);
+                    i.Cells[2].Value = prid.Naziv;
+
+                }
+            }
+
+            if (result2 != null)
+            {
+                foreach (DataGridViewRow i in dgvIzvrsene.Rows)
+                {
+                    var prid = await _odjel.GetById<OdjelVM>(result2[i.Index].OdjelID);
+                    i.Cells[2].Value = prid.Naziv;
+
+                }
+            }
         }
 
         private  void dgvAktivne_CellClick(object sender, DataGridViewCellEventArgs e)
